28911e1e305abca271a732b98b163ffa92ef203e
Caian R. Ertl <hi@caian.org>
2021-04-18 04:36:19 -0300

    chore: setup configs for typescript + webpack

---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import * as path from 'path'
+
+import * as webpack from 'webpack'
+import TerserPlugin from 'terser-webpack-plugin'
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'cli.ts'),
+  target: 'node',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: true
+      })
+    ],
+    mangleExports: 'size',
+    moduleIds: 'size',
+    chunkIds: 'size',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
+        exclude: /node-modules/
+      }
+    ]
+  },
+  plugins: [new webpack.IgnorePlugin({ resourceRegExp: /aws-sdk/ })]
+}
+
+export default config
