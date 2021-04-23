b906f7a724c10e19c55e25748ff0b6c7749d4c61
Caian R. Ertl <hi@caian.org>
2021-04-23 06:26:46 -0300

    chore: export to lib/ instead of dist/

--- webpack.config.ts
+++ webpack.config.ts
@@ -19,7 +19,7 @@ const config: webpack.Configuration = {
     chunkIds: 'size',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'lib'),
     filename: 'cli.js'
   },
   resolve: {
