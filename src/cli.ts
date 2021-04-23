6a22bf1654b077518b6866f0401c21fc04639907
Caian R. Ertl <hi@caian.org>
2021-04-23 06:01:40 -0300

    feat: detect the OS and run the appropriate binary

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,35 @@
-console.log('hello')
+import path from 'path'
+import { spawn } from 'child_process'
+
+function getPlatformBin (): string {
+  switch (process.platform) {
+    case 'win32':
+      return 'vrelease-windows.exe'
+
+    case 'linux':
+      return 'vrelease-linux'
+
+    case 'darwin':
+      return 'vrelease-macos'
+
+    default:
+      throw new Error(`unsupported platform ${process.platform}`)
+  }
+}
+
+;((): void => {
+  try {
+    if (process.arch !== 'x64') {
+      throw new Error(`unsupported architecture ${process.arch}`)
+    }
+
+    const file = getPlatformBin()
+
+    const binPath = path.resolve(__dirname, '..', 'bin', file)
+    const input = process.argv.slice(2)
+
+    spawn(binPath, input, { stdio: 'inherit' }).on('exit', process.exit)
+  } catch (e) {
+    process.exit(2)
+  }
+})()
