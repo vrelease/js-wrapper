7e5d1b860cddb180175285bce90897cbddf67dbd
Caian Ertl <hi@caian.org>
2022-01-25 05:58:56 -0300

    feat: wrap logic inside function

--- src/cli.ts
+++ src/cli.ts
@@ -2,15 +2,18 @@
 
 import { VRelease } from './wrapper'
 
-if (process.argv.length > 2) {
+function runCLI (): void {
   const input = process.argv.slice(2).filter((i) => i.length > 0)
 
-  if (input.length > 0) {
-    new VRelease(input).run().catch((e) => {
-      const m = e instanceof Error ? e.message : JSON.stringify(e)
-
-      console.error(m)
-      process.exit(2)
-    })
+  if (input.length === 0) {
+    return
   }
+
+  new VRelease(input).run().catch((e) => {
+    const m = e instanceof Error ? e.message : JSON.stringify(e)
+    console.error(m)
+    process.exit(2)
+  })
 }
+
+runCLI()
